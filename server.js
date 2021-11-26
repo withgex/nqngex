5218720a514cbae240e9fad7a6d8f80f53e75085
gex <89141668+withgex@users.noreply.github.com>
2021-11-26 21:32:12 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const express = require('express')
 const server = express();
 
 server.all('/', (req, res) => {
-    res.send('El bot sigue encendido: https://linkfly.to/withgex');
+    res.send('El bot sigue encendido: https://withgex.wixsite.com/tools');
+    res.send('El bot sigue encendido: https://dsc.gg/gex');
 });
  
 module.exports = () => {
     server.listen(3000, () => {
-        console.log('Servidor Listo: https://linkfly.to/withgex');
+        console.log('Servidor Listo: https://withgex.wixsite.com/tools/upmonitor');
     });
     return true;
 }
